5d9764713d3e250a4682780c586abb2e75f6382c
Tarun <tarunm2108@gmail.com>
2023-07-11 16:31:00 +0530

    Refactor code

--- lib/utils/utility.dart
+++ lib/utils/utility.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
 class Utility {
@@ -7,4 +8,14 @@ class Utility {
     Colors.purple,
     Colors.blue
   ];
+
+  static Future<bool> checkNetwork() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
